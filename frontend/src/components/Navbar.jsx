70f4013f0e4befb6dad4047a4c8996dbb0b2573b
nooby6 <giftaudrine1@gmail.com>
2024-11-04 23:11:12 +0300

    fix(Navbar): correct logo image source syntax in Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 // Desc: Navbar component for the app. This component is imported in App.jsx and rendered in the main app component.
 import './Navbar.css'
-import  { assets } from '../../assets/assets'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img src="{assets.logo}" alt="" className="log" />
+      <img src={assets.logo} alt="" className="log" />
       <ul className='navbar-menu'>
-
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
       </ul>
     </div>
   )
