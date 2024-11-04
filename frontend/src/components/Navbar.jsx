e5768865f94e37e334f3990a338935c860dec370
nooby6 <giftaudrine1@gmail.com>
2024-11-04 23:09:07 +0300

    fix(Navbar): update logo image source syntax and add navbar menu structure

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,14 @@
+// Desc: Navbar component for the app. This component is imported in App.jsx and rendered in the main app component.
 import './Navbar.css'
 import  { assets } from '../../assets/assets'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt=''/>
+      <img src="{assets.logo}" alt="" className="log" />
+      <ul className='navbar-menu'>
+
+      </ul>
     </div>
   )
 }
