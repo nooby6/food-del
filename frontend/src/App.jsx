28e44f54a82c0d025b576d106a8b2492043e358b
nooby6 <giftaudrine1@gmail.com>
2024-11-05 19:26:43 +0300

    feat(App): implement routing for Home, Cart, and PlaceOrder components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/Cart' element={<Cart/>} />
+        <Route path='orders' element={<PlaceOrder/>} />
+      </Routes>
     </div>
   )
 }
