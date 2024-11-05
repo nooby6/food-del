09556797f42ac015b120a8546fde4f4d153d1c51
nooby6 <giftaudrine1@gmail.com>
2024-11-05 15:37:26 +0300

    feat(Navbar): add button styles and implement state for active menu item

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,20 @@
 // Desc: Navbar component for the app. This component is imported in App.jsx and rendered in the main app component.
+import React, { useState } from 'react';
 import './Navbar.css';
 import { assets } from '../../assets/assets';
 
 const Navbar = () => {
+
+  const [Menu, setMenu] = useState('Home');
+
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className="log" />
       <ul className='navbar-menu'>
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile App</li>
-        <li>Contact Us</li>
+        <li className={Menu==="Home"?"active":""}>Home</li>
+        <li className={Menu===""}>Menu</li>
+        <li className={Menu===""}>Mobile App</li>
+        <li className={Menu===""}>Contact Us</li>
       </ul>
       <div className='navbar-right'>
         <img src={assets.search_icon} alt="" />
