73c105c0277106fe52bc86300d994bc4e270504d
nooby6 <giftaudrine1@gmail.com>
2024-11-06 14:03:45 +0300

    feat(Home): integrate ExploreMenu component into Home page; update import path for assets

--- frontend/src/components/Navbar/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/Navbar/ExploreMenu/ExploreMenu.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import './ExploreMenu.css'
-import { menu_list } from '../../assets/assets'
+import { menu_list } from "@assets/assets";
 
 const ExploreMenu = () => {
   return (
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import './Home.css'
 import Header from '../../components/Navbar/Header/Header'
+import ExploreMenu from '../../components/Navbar/ExploreMenu/ExploreMenu'
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <ExploreMenu/>
     </div>
   )
 }
