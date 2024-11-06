1b95cec6861f938fe66556a4840e840b8bdd33be
nooby6 <giftaudrine1@gmail.com>
2024-11-06 11:46:36 +0300

    feat(Header): enhance Header component with additional content and styling; integrate Header in Home page

--- frontend/src/components/Navbar/Header/Header.jsx
+++ frontend/src/components/Navbar/Header/Header.jsx
@@ -3,7 +3,12 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div>
+    <div className='header'>
+      <div className='header__info'>
+        <h2>Order your favourite food here</h2>
+        <p>Order your favourite food from your favourite restaurant and enjoy the meal with your family and friends</p>
+        <button>View Menu</button>
+      </div>
       
     </div>
   )
