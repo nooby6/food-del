e69c59444adb6ff38e8496e782309687e81cd3c6
nooby6 <giftaudrine1@gmail.com>
2024-11-06 13:26:01 +0300

    feat(Header): add header contents with styling and fade-in animation; create ExploreMenu component with initial structure

--- frontend/src/components/Navbar/Header/Header.jsx
+++ frontend/src/components/Navbar/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-      <div className='header__info'>
+      <div className='header-contents'>
         <h2>Order your favourite food here</h2>
         <p>Order your favourite food from your favourite restaurant and enjoy the meal with your family and friends</p>
         <button>View Menu</button>
