4b5c5dd37518caaef4d08575d402ff1ef2bdcd6d
nooby6 <giftaudrine1@gmail.com>
2024-11-06 20:31:42 +0300

    feat(Fooditem): enhance Fooditem component with styling and rating display

--- frontend/src/components/Fooditem/Fooditem.jsx
+++ frontend/src/components/Fooditem/Fooditem.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Fooditem.css'
+import { assets } from '../../assets/assets' // Adjust the path as necessary
 
 const Fooditem = ({id,name,price,description,image}) => {
   return (
-    <div>
-      
+    <div className='food-item'>
+        <div className="food-item-container">
+            <img className='food-item-image' src={image} alt="" />
+        </div>
+        <div className="food-item-info">
+            <div className="food-item-name-rating">
+                <img src={assets.rating_stars} alt="Rating Stars" />
+            </div>
+        </div>
+      <p className='food-item-description'>{description}</p>
+      <p className="food-item-price">Ksh{price}</p>
     </div>
   )
 }
