47edae170b26542f75936f4677b5321ccc4b4bbb
nooby6 <giftaudrine1@gmail.com>
2024-11-07 08:45:28 +0300

    feat(Fooditem): add item count state and conditional rendering for add icon

--- frontend/src/components/Fooditem/Fooditem.jsx
+++ frontend/src/components/Fooditem/Fooditem.jsx
@@ -3,10 +3,19 @@ import './Fooditem.css'
 import { assets } from '../../assets/assets'
 
 const Fooditem = ({id,name,price,description,image}) => {
+
+    const[itemCount, setItemCount] = React.useState(0);
+
   return (
     <div className='food-item'>
         <div className="food-item-container">
             <img className='food-item-image' src={image} alt="" />
+            { !itemCount 
+                ?<img src={assets.add_icon_white} alt="Add Icon" />
+                :<div className='food-item-counter'>
+
+                </div> 
+            }
         </div>
         <div className="food-item-info">
             <div className="food-item-name-rating">
